9ac2b014338caf01cfec678b5566a270e9b0e148
Johnny Shen <jshen130@gmail.com>
2013-07-12 17:39:05 -0700

    updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync("index.html").toString();
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
